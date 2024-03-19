f73ae547ae0e5a9ee248997570431430da769a6a
solikul arip <solikul.arip@gmail.com>
2024-03-19 11:12:35 +0700

    menambah log

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,24 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+    function getSqlWithBindings($query)
+    {
+        return vsprintf(str_replace('?', '%s', $query->sql), collect($query->bindings)->map(function ($binding) {
+            return is_numeric($binding) ? ($binding === null ? 'null' : $binding) : "'{$binding}'";
+        })->toArray());
+    }
+
     public function boot(): void
     {
-        //
+        // Log sql to artisan
+        DB::listen(function ($query) {
+            $sql = $this->getSqlWithBindings($query);
+            if (strstr($sql, 'sessions') === false) {
+                $time = $query->time;
+                file_put_contents('php://stdout', "[SQL]" . date("Y-m-d H:i:s") . " {$sql} \n" .
+                    //   "      bindings:\t".json_encode($bindings)."\n".
+                    "      Time:\t{$time} milliseconds\n");
+            }
+        });
     }
 }
