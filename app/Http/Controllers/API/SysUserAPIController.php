f50198fe5cbbfdd18f73f8571a89755836ee20dd
muhammad fakhruddin <m.fakhruddin.arozi@gmail.com>
2024-05-18 14:57:52 +0700

    a

--- app/Http/Controllers/API/SysUserAPIController.php
+++ app/Http/Controllers/API/SysUserAPIController.php
@@ -1,107 +1,15 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use App\Http\Controllers\BaseResourceController;
 
 /**
- * Class SysGroupMenuAPIController
+ * Class SysUserAPIController
  */
 class SysUserAPIController extends BaseResourceController
 {
     public function __construct()
     {
         $this->model = new \App\Models\SysUser;
-    }
-
-    public function index(Request $request): JsonResponse
-    {
-        return $this->respond(['success' => 'succeessss']);
-        //     $search = $request->get('q');
-        //     if ($search) {
-        //         if (!empty($search['nama']))
-        //             $search['nama'] = "%" . $search['nama'] . "%";
-        //         if (!empty($search['kode']))
-        //             $search['kode'] = "%" . $search['kode'] . "%";
-        //     }
-        //     // $filter = $request->get('q');
-        //     $page = $request->get('page') ?? 1;
-        //     $limit = $request->get('pagesize') ?? $this->limit;
-        //     $db = $this->model->search($search);
-
-        //     $orderby = $request->get('order');
-        //     if ($orderby) {
-        //         $orderby = explode(",", $orderby);
-        //         if (!is_array($orderby))
-        //             $orderby = array($orderby);
-
-        //         foreach ($orderby as $v) {
-        //             list($column, $sc) = explode(" ", $v);
-
-        //             $db = $db->orderBy($column, $sc);
-        //         }
-        //     } else if ($this->model->primaryKey) {
-        //         $db = $db->orderBy($this->model->primaryKey);
-        //     }
-
-        //     // if($filter)
-        //     // 	$db = $db->where($filter);
-        //     $data = $db->paginate($limit);
-        //     // $pagination = [
-        //     // 	'currentPage' => $this->model->pager->getCurrentPage(),
-        //     // 	'totalPage' => $this->model->pager->getPageCount(),
-        //     // ];
-        //     // dd($data->items);
-        //     return $this->respond([
-        //         'page' => $data->currentPage(),
-        //         'page_size' => $data->perPage(),
-        //         'data' => $data->items(),
-        //         'total_page' => ceil($data->total() / $limit),
-        //         'total_records' => $data->total()
-        //     ]);
-        //     // return $this->respond([
-        //     //     'page' => $this->model->pager->getCurrentPage(),
-        //     //     'page_size' => $limit,
-        //     //     'data' => $data,
-        //     //     'total_page' => $this->model->pager->getPageCount(),
-        //     //     'total_records' => $this->model->pager->getDataCount()
-        //     // ]);
-    }
-
-    public function create(Request $request): JsonResponse
-    {
-        $data = $request->all();
-        if ($data['password'] <> $data['password_konfirmasi']) {
-            return $this->failValidationError("Password tidak sama dengan password konfirmasi");
-        }
-
-        if (!$data['password'] || !$data['name'] || !$data['email'])
-            return $this->fail(["errorr" => "Masukan data dengan lengkap !"]);
-
-        $ret = $this->model->where('name', $data['name'])->get();
-        if (count($ret)) {
-            return $this->fail(['errorr' => 'nama sudah ada']);
-        }
-
-        $this->model->create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($request->password)
-        ]);
-
-        // if (!$id = $this->model->insert($data)) {
-        //     return $this->fail($this->model->errors());
-        // }
-
-        // unset($data->password);
-        // unset($data->salt);
-
-        // $data->id_user = $id;
-        // $data->id_driver = $id;
-
-        return $this->respondCreated($data, 'data created');
     }
 }
--- app/Models/SysUser.php
+++ app/Models/SysUser.php
@@ -2,46 +2,73 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class SysUser extends Authenticatable
+class SysUser extends BaseModel
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    public $table = 'sys_user';
 
-    public $primaryKey = "id_user";
-    protected $table = "sys_user";
-    
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
+    protected $primaryKey = 'id_user';
+
+    public $fillable = [
         'name',
         'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
+        'email_verified_at',
         'password',
         'remember_token',
+        'last_ip',
+        'last_login',
+        'created_by',
+        'updated_by',
+        'created_by_desc',
+        'modified_by_desc',
+        'salt',
+        'deleted_by',
+        'deleted_by_desc',
+        'id_pegawai'
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
+        'name' => 'string',
+        'email' => 'string',
         'email_verified_at' => 'datetime',
+        'password' => 'string',
+        'remember_token' => 'string',
+        'last_ip' => 'string',
+        'created_by_desc' => 'string',
+        'modified_by_desc' => 'string',
+        'salt' => 'string',
+        'deleted_by_desc' => 'string'
     ];
+
+    public array $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|max:255',
+        'email_verified_at' => 'nullable',
+        'password' => 'required|string|max:255',
+        'remember_token' => 'nullable|string|max:100',
+        'created_at' => 'nullable',
+        'updated_at' => 'nullable',
+        'last_ip' => 'nullable|string|max:30',
+        'last_login' => 'nullable',
+        'created_by' => 'nullable',
+        'updated_by' => 'nullable',
+        'created_by_desc' => 'nullable|string|max:200',
+        'modified_by_desc' => 'nullable|string|max:200',
+        'salt' => 'nullable|string|max:20',
+        'deleted_at' => 'nullable',
+        'deleted_by' => 'nullable',
+        'deleted_by_desc' => 'nullable|string|max:200',
+        'id_pegawai' => 'nullable'
+    ];
+
+    public function idPegawai(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(\App\Models\MtSdmPegawai::class, 'id_pegawai');
+    }
+
+    public function sysUserGroups(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(\App\Models\SysUserGroup::class, 'id_user');
+    }
 }
