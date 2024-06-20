7e265f8a0852b5bf9d889614f9e5e04254a8a773
solikul arip <solikul.arip@gmail.com>
2024-06-20 10:40:42 +0700

    penyerderhanaan rupiah

--- app/helper/helper.php
+++ app/helper/helper.php
@@ -1,12 +1,22 @@
 <?php
 
-function rupiah ($number){
-    if(is_array($number)) return null;
-    if(!$number) return null;
+function rupiah1($number)
+{
+  if (is_array($number)) return null;
+  if (!$number) return null;
 
-    if (strlen(round($number)) === strlen($number))
-		return number_format($number, 0, ",", ".");
+  if (strlen(round($number)) === strlen($number))
+    return number_format($number, 0, ",", ".");
 
-    return number_format($number, 2, ",", ".");
+  return number_format($number, 2, ",", ".");
 }
 
+function rupiah($number)
+{
+  if ($number > 1000000000000)
+    return rupiah1(round($number / 1000000000000)) . ' T';
+  if ($number > 1000000000)
+    return rupiah1(round($number / 1000000000)) . ' M';
+  if ($number > 1000000000000)
+    return rupiah1(round($number / 1000000)) . ' Jt';
+}
